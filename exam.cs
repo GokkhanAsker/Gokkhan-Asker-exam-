0eac1c4602dc9546810cc46df0feacb9719ea712
GokkhanAsker <99753192+GokkhanAsker@users.noreply.github.com>
2022-05-11 19:26:34 +0300

    add exam.cs add modified readme

---
+++ exam.cs
@@ -0,0 +1,14 @@
+﻿string[] stringArray = { "The", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog" };// объявляем первоначальный массив
+var count = string.Empty;// обЪявляем пустую переменную
+for (int index = 0; index < stringArray.Length; index++)  // цикл. Проходим весь массив
+{
+    if (stringArray[index].Length <= 3) // проверяем данный индекс массива, количество символов должно быть меньше либо равно трем
+    {
+        count += "'" + stringArray[index] + "'"; //если количество символов меньше либо равно трем, пишем данный индекс в переменную count 
+    }
+}
+char[] newstringArray = count.ToCharArray(); //переменную переводим в массив
+System.Console.WriteLine("Первоначальный массив");
+Console.WriteLine("[{0}]", string.Join(",", stringArray));//печатаем первоначальный массив
+Console.WriteLine("Обработанный массив: ");
+Console.WriteLine("[{0}]", string.Join("", newstringArray));//печатаем обработанный массив
